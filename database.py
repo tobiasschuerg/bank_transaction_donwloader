fb7441dc9e47ee70a5800b75059872315e94c6a2
Tobias Schürg <tobiasschuerg@gmail.com>
2023-04-29 12:52:53 +0200

    store transactions in a sqlite database instead of csv

---
+++ database.py
@@ -0,0 +1,31 @@
+import os
+import sqlite3
+
+
+def connect_to_db():
+    db_folder = "transactions"
+    if not os.path.exists(db_folder):
+        os.makedirs(db_folder)
+    db_filename = "transactions.db"
+    db_filepath = os.path.join(db_folder, db_filename)
+    conn = sqlite3.connect(db_filepath)
+    c = conn.cursor()
+
+    # Create tables for banks and transactions if they don't exist
+    c.execute('''CREATE TABLE IF NOT EXISTS banks (
+                    bankName text PRIMARY KEY,
+                    iban text NOT NULL)''')
+
+    c.execute('''CREATE TABLE IF NOT EXISTS transactions (
+                    transactionId text PRIMARY KEY,
+                    bankName text NOT NULL,
+                    bookingDate date NOT NULL,
+                    valueDate date,
+                    amount real NOT NULL,
+                    currency text,
+                    description text,
+                    creditorName text,
+                    creditorAccount text,
+                    debtorAccount text,
+                    FOREIGN KEY (bankName) REFERENCES banks(bankName))''')
+    return conn
--- transactions.py
+++ transactions.py
@@ -1,47 +1,43 @@
-import csv
-import os
+import database
 
 
 def store_transactions(transactions, bank_name, iban):
-    # Save transactions to CSV
-    csv_folder = "transactions"
-    if not os.path.exists(csv_folder):
-        os.makedirs(csv_folder)
-    csv_filename = f"{iban}_{bank_name}.csv"
-    csv_filepath = os.path.join(csv_folder, csv_filename)
-    existing_transactions = []
-    if os.path.exists(csv_filepath):
-        # If the file already exists, read the existing transactions
-        with open(csv_filepath, "r", newline="", encoding="utf-8") as csvfile:
-            reader = csv.DictReader(csvfile)
-            existing_transactions = [row for row in reader]
+    # Connect to SQLite database
+    conn = database.connect_to_db()
+    c = conn.cursor()
 
-    # Remove duplicate transactions from the list of new transactions
-    new_transactions = []
+    # Insert the bank information into the database if it doesn't exist
+    c.execute('''SELECT bankName FROM banks WHERE bankName = ?''', (bank_name,))
+    result = c.fetchone()
+    if result is None:
+        c.execute('''INSERT INTO banks (bankName, iban)
+                    VALUES (?, ?)''', (
+            bank_name,
+            iban
+        ))
+
+    # Insert new transactions into the database
     for transaction in transactions:
-        if not any(t.get("transactionId") == transaction.get("transactionId") and t.get("bookingDate") == transaction.get("bookingDate") for t in existing_transactions):
-            new_transactions.append(transaction)
-
-    # Append new transactions to the existing file
-    with open(csv_filepath, "a", newline="", encoding="utf-8") as csvfile:
-        fieldnames = ["transactionId", "bookingDate", "valueDate", "amount", "currency", "description", "creditorName",
-                      "creditorAccount", "debtorAccount"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        if os.path.getsize(csv_filepath) == 0:
-            # If the file is empty, write the header
-            writer.writeheader()
-        for transaction in new_transactions:
-            transaction_id = transaction.get("transactionId", transaction.get("internalTransactionId"))
-            writer.writerow({
-                "transactionId": transaction_id,
-                "bookingDate": transaction.get("bookingDate"),
-                "valueDate": transaction.get("valueDate", None),
-                "amount": transaction["transactionAmount"]['amount'],
-                "currency": transaction['transactionAmount']["currency"],
-                "description": transaction.get("remittanceInformationUnstructured",
-                                               transaction.get("remittanceInformationUnstructuredArray")),
-                "creditorName": transaction.get("creditorName", ""),
-                "creditorAccount": transaction.get("creditorAccount", {}).get("iban", None),
-                "debtorAccount": transaction.get("debtorAccount", {}).get("iban", None),
-            })
-    print(f"Transactions saved to {csv_filepath}")
+        transaction_id = transaction.get("transactionId", transaction.get("internalTransactionId"))
+        c.execute('''SELECT transactionId FROM transactions WHERE transactionId = ?''', (transaction_id,))
+        result = c.fetchone()
+        if result is None:
+            c.execute('''INSERT INTO transactions (transactionId, bankName, bookingDate, valueDate, amount, currency, description, creditorName, creditorAccount, debtorAccount)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (
+                transaction_id,
+                bank_name,
+                transaction.get("bookingDate"),
+                transaction.get("valueDate", None),
+                transaction["transactionAmount"]['amount'],
+                transaction['transactionAmount']["currency"],
+                transaction.get("remittanceInformationUnstructured",
+                                transaction.get("remittanceInformationUnstructuredArray")),
+                transaction.get("creditorName", ""),
+                transaction.get("creditorAccount", {}).get("iban", None),
+                transaction.get("debtorAccount", {}).get("iban", None)
+            ))
+        else:
+            print(f"Transaction with ID {transaction_id} already exists in the database")
+    conn.commit()
+    conn.close()
+    print(f"Transactions saved to database")
